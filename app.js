488c9116bc56dd17c24768a7a01ccd23909f95e8
Josh Dudley <joshdudley.vt@gmail.com>
2019-08-09 04:41:29 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+let x = 16;
+let gridContainer = document.querySelector('#gridContainer');
+
+
+//Create the grid
+function createGrid() {
+    for (let  i= 0; i < x * x; i++) {
+        let gridBox = document.createElement('div');
+        gridBox.classList.add('gridBox');
+       
+        gridContainer.appendChild(gridBox);
+    
+        
+        const gridBoxes = document.querySelectorAll('.gridBox') [i];
+            gridBoxes.style.width = `${600/x}px`;
+            gridBoxes.style.height = `${600/x}px`;
+
+            gridBoxes.addEventListener('mouseover', function (e) {
+                this.style.background = 'black';
+            })
+    };
+};
+
+createGrid();
+
+
+//Create reset button
+let sketchControls = document.querySelector('#sketchControls');
+let resetButton = document.createElement('button');
+resetButton.classList.add('resetButton');
+resetButton.textContent = 'reset grid';
+sketchControls.appendChild(resetButton);
+
+//reset button event
+resetButton.addEventListener('click', function (e) {
+    if (confirm('are you sure?')) {
+
+        while(gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
+
+        createGrid();
+    }
+});
+
+
+//Create resize button
+let resizeButton = document.createElement('button');
+resizeButton.classList.add('resizeButton');
+resizeButton.textContent = 'resize grid';
+sketchControls.appendChild(resizeButton);
+
+//resize button event
+resizeButton.addEventListener('click', function (e) {
+    gridContainer.innerHTML = '';
+    x = parseInt(prompt('how many grid squares per side?'))
+    createGrid();
+})
+
+
+
+
